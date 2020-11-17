dd471a659f41a645cf2ec675e62ce02301ceb446
Nahid Hossain <oikantik@gmail.com>
2020-11-17 11:31:23 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    origin: "*"
+}));
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "it works"})
+})
+
+app.listen(8000, () => console.log('listening'))
